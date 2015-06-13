00dcc4bbcf5011a96c71edfed464875ae1f380f7
romeOz <serggalka@gmail.com>
2015-06-13 20:29:57 +0300

    Remove create config to bootstrap.

--- apps/common/migrations/bootstrap.php
+++ apps/common/migrations/bootstrap.php
@@ -15,26 +15,6 @@ if (!$config = require(dirname(dirname(__DIR__)) . '/common/configs/configs.php'
     die('configs is empty/not found');
 }
 
-if ($env = array_slice($argv, 1)) {
-
-    $db = [
-        'class' => \rock\db\Connection::className(),
-        'username' => $env[0],
-        'password' => $env[1],
-        'charset' => 'utf8',
-        'dsn' => 'mysql:host=localhost;dbname=rockdemo;charset=utf8',
-        'tablePrefix' => 'spt_',
-        'aliasSeparator' => '__',
-        'enableSchemaCache' => true,
-        //'enableQueryCache'  => true
-    ];
-    $path = dirname(dirname(__DIR__)) . '/common/configs/classes.php';
-    $classes = require($path);
-    $config['components']['db'] = $classes['db'] = $db;
-    file_put_contents($path, "<?php\nreturn " .var_export($classes, true) . ';');
-}
-
-
 Rock::$components = $config['components'];
 unset($config['components']);
 Rock::$config = $config;

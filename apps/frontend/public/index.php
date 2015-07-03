3ad2235b3c75a79d75972ec35cb7cae4cf795965
romeOz <serggalka@gmail.com>
2015-07-03 22:53:35 +0300

    Moved directory `public` to apps/frontend

--- apps/frontend/public/index.php
+++ apps/frontend/public/index.php
@@ -1,22 +1,20 @@
 <?php
-use rock\Rock;
-
 error_reporting(-1);
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
     die('need to use PHP version 5.4.x or greater');
 }
 
-require(dirname(__DIR__) . '/vendor/autoload.php');
+require(dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php');
 
 defined('ROCK_DEBUG') or define('ROCK_DEBUG', true);
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
-Rock::$app = new Rock();
+\rock\Rock::$app = new \rock\Rock();
 
 // catch error
 \rock\exception\ErrorHandler::register();
 
-$config = require(dirname(__DIR__) .'/apps/frontend/configs/configs.php');
+$config = require(dirname(__DIR__) .'/configs/configs.php');
 
 // bootstrap application
 \rock\Rock::bootstrap($config);

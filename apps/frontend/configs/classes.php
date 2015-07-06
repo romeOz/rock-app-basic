74f2f503eacf04a12a6a3ae85a84dcb3d96b43e5
romeOz <serggalka@gmail.com>
2015-07-06 15:25:34 +0300

    Remove dependencies: jquery and bootstrap.js

--- apps/frontend/configs/classes.php
+++ apps/frontend/configs/classes.php
@@ -51,39 +51,27 @@ return [
         'head' => '<!DOCTYPE html><html lang="'.\rock\Rock::$app->language .'" data-ng-app="app">',
         'cssFiles' => [
             rock\template\Template::POS_HEAD => [
-                '<link href="/assets/css/demo.css" rel="stylesheet"/>'
+                '<link href="/assets/css/demo.min.css" rel="stylesheet"/>'
             ],
         ],
 
         'jsFiles' => [
             rock\template\Template::POS_END => [
                 // vendor
-                '<script src="/assets/js/jquery.min.js"></script>',
-                '<script src="/assets/js/bootstrap.min.js"></script>',
+                //'<script src="/assets/js/jquery.min.js"></script>',
+                //'<script src="/assets/js/bootstrap.min.js"></script>',
                 '<!--[if !(IE) | (gt IE 8) ]>-->',
 
-                '<script src="/assets/js/vendor/underscore/underscore.js"></script>',
-                '<script src="/assets/js/vendor/angular/angular.min.js"></script>',
-                '<script src="/assets/js/vendor/angular-animate/angular-animate.min.js"></script>',
-                '<script src="/assets/js/vendor/angular-bootstrap/ui-bootstrap.min.js"></script>',
-                '<script src="/assets/js/vendor/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>',
-                '<script src="/assets/js/vendor/angular-translate/angular-translate.min.js"></script>',
+//                '<script src="/assets/js/vendor/underscore/underscore.js"></script>',
+//                '<script src="/assets/js/vendor/angular/angular.min.js"></script>',
+//                '<script src="/assets/js/vendor/angular-animate/angular-animate.min.js"></script>',
+//                '<script src="/assets/js/vendor/angular-bootstrap/ui-bootstrap.min.js"></script>',
+//                '<script src="/assets/js/vendor/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>',
+//                '<script src="/assets/js/vendor/angular-translate/angular-translate.min.js"></script>',
+//                '<script src="/assets/js/vendor/angular-rock/angular-rock.min.js"></script>',
 
-
-                // core
-                '<script src="/assets/js/app.js"></script>',
-                '<script src="/assets/js/core/core.js"></script>',
-                '<script src="/assets/js/core/helpers.js"></script>',
-                '<script src="/assets/js/core/services.js"></script>',
-                '<script src="/assets/js/core/filters.js"></script>',
-                '<script src="/assets/js/core/directives.js"></script>',
-                '<script src="/assets/js/core/notifications/notification.js"></script>',
-                '<script src="/assets/js/core/notifications/notification.controllers.js"></script>',
-                '<script src="/assets/js/core/notifications/notification.services.js"></script>',
-                '<script src="/assets/js/core/forms/forms.js"></script>',
-                '<script src="/assets/js/core/forms/forms.directives.js"></script>',
-                '<script src="/assets/js/core/forms/forms.services.js"></script>',
-                '<script src="/assets/js/core/forms/forms.controllers.js"></script>',
+                // app
+                '<script src="/assets/js/app.min.js"></script>',
 
                 '<![endif]-->'
             ]
